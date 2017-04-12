8c383ef8f857657c95af1c5dc7bc9ecfd90ab1f7
JohnAbr <UF974943@md.gasnatural.com>
2017-04-12 08:59:41 +0300

    Game over

--- app/src/main/java/john/abr/md/circle/GameManager.java
+++ app/src/main/java/john/abr/md/circle/GameManager.java
@@ -3,31 +3,104 @@ package john.abr.md.circle;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
-/**
- * Created by UF974943 on 06.04.2017.
- */
+import java.util.ArrayList;
 
 public class GameManager {
+    public static final int MAX_CIRCLES = 10;
     private MainCircle mainCircle;
-    private Paint paint;
+    private ArrayList<EnemyCircle> circles;
+    private CanvasView canvasView;
+    private static int width;
+    private static int height;
 
-    public GameManager() {
+
+    public GameManager(CanvasView canvasView, int w, int h) {
+        this.canvasView = canvasView;
+        width = w;
+        height = h;
         initMainCircle();
-        initPaint();
+        initEnemyCircles();
     }
 
-    private void initPaint() {
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.FILL);
+    private void initEnemyCircles() {
+        circles = new ArrayList<EnemyCircle>();
+        SimpleCircle mainCircleArea = mainCircle.getCircleArea();
+        for (int i = 0; i < MAX_CIRCLES; i++) {
+            EnemyCircle circle;
+            do {
+                circle = EnemyCircle.getRandomCircle();
+            } while (circle.isIntersect(mainCircleArea));
+            circles.add(circle);
+        }
+        calculateAndSetCircleColor();
+    }
+
+    private void calculateAndSetCircleColor() {
+        for (EnemyCircle circle : circles) {
+            circle.setEmenyOrFoodColorDependsOn(mainCircle);
+        }
+    }
+
+    public static int getWidth() {
+        return width;
+    }
+
+    public static int getHeight() {
+        return height;
     }
 
     private void initMainCircle() {
-        mainCircle = new MainCircle(200, 500);
+        mainCircle = new MainCircle(width / 2, height / 2);
     }
 
-    public void onDraw(Canvas canvas) {
-        canvas.drawCircle(mainCircle.getX(), mainCircle.getY(), mainCircle.getRadius(), paint);
+    public void onDraw() {
+        canvasView.drawCircle(mainCircle);
+        for (EnemyCircle circle : circles) {
+            canvasView.drawCircle(circle);
+        }
+    }
+
+    public void onTouchEvent(int x, int y) {
+        mainCircle.moveMainCircleWhenTouchAt(x, y);
+        checkCollision();
+        moveCircles();
+    }
+
+    private void checkCollision() {
+        SimpleCircle circleForDell = null;
+        for (EnemyCircle circle : circles) {
+            if (mainCircle.isIntersect(circle)) {
+                if (circle.isSmallerThat(mainCircle)) {
+                    mainCircle.growRadius(circle);
+                    circleForDell = circle;
+                    calculateAndSetCircleColor();
+                    break;
+                } else {
+                    gameEnd("YOU LOSE!");
+                    return;
+                }
+            }
+
+        }
+        if (circleForDell != null) {
+            circles.remove(circleForDell);
+        }
+        if (circles.isEmpty()){
+            gameEnd("YOU WIN!");
+        }
+    }
+
+    private void gameEnd(String text) {
+        canvasView.showMessage(text);
+        mainCircle.initRadius();
+        initEnemyCircles();
+        canvasView.redraw();
+    }
+
+    private void moveCircles() {
+        for (EnemyCircle circle : circles){
+            circle.moveOnStep();
+        }
     }
 }
 
---
+++ app/src/main/java/john/abr/md/circle/ICanvasView.java
@@ -0,0 +1,11 @@
+package john.abr.md.circle;
+
+
+
+public interface ICanvasView {
+    void drawCircle(SimpleCircle circle);
+
+    void redraw();
+
+    void showMessage(String text);
+}
